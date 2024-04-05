63532911d1cda8085df0b36c3ece599969cacef2
Rohit Rohit <rohitsaroha125@gmail.com>
2024-04-05 14:31:57 +0530

    Joined Companies with Jobs

--- controllers/jobControllers.js
+++ controllers/jobControllers.js
@@ -1,7 +1,12 @@
 import prisma from "../lib/client.js";
 
 export async function getJobs() {
-  const allJobs = await prisma.job.findMany();
+  const allJobs = await prisma.job.findMany({
+    relationLoadStrategy: "join",
+    include: {
+      company: true,
+    },
+  });
   console.log("jobs are ", allJobs);
   return allJobs;
 }
--- resolvers.js
+++ resolvers.js
@@ -2,6 +2,12 @@ import { getJobs } from "./controllers/jobControllers.js";
 
 export const resolvers = {
   Query: {
-    job: getJobs,
+    job: () => getJobs(),
   },
+
+  //   Job: {
+  //     createdAt: (job) => {
+  //       return job.createdAt;
+  //     },
+  //   },
 };
