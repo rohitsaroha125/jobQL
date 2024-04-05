c18d406b54abea61f88adcb457f6f899bdad883a
Rohit Rohit <rohitsaroha125@gmail.com>
2024-04-05 14:53:46 +0530

    Fetched jobs data on frontend

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,12 +1,42 @@
 import Image from "next/image";
 import { jobs } from "@/lib/fake-data";
 import JobList from "@/components/JobList";
+import axios from "axios";
+import { API_URL } from "@/lib/config";
 
-export default function Home() {
+export default async function Home() {
+  const { data } = await axios.post(
+    API_URL,
+    JSON.stringify({
+      query: `
+      query{
+        job {
+          id
+          title
+          description
+          createdAt
+          updatedAt
+          company {
+            id
+            name
+          }
+        }
+      }
+    `,
+    }),
+    {
+      headers: {
+        "content-type": "application/json",
+      },
+    }
+  );
+
+  console.log("data is ", data.data);
+
   return (
     <div>
       <h1 className="title">Job Board</h1>
-      <JobList jobs={jobs} />
+      <JobList jobs={data.data.job} />
     </div>
   );
 }
--- server.js
+++ server.js
@@ -4,11 +4,13 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import express from "express";
 import { readFile } from "node:fs/promises";
 import { resolvers } from "./resolvers.js";
+import cors from "cors";
 
 const PORT = 5000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const typeDefs = await readFile("./schema.graphql", "utf-8");
